d49ef3e1e6b8e7b60cef032bf9999aec688bdebb
antboj <andrejbojic.03@gmail.com>
2019-05-16 15:06:18 +0200

    Added MakePermissions method

--- aspnet-core/src/TestProject.Core/Authorization/PermissionNames.cs
+++ aspnet-core/src/TestProject.Core/Authorization/PermissionNames.cs
@@ -3,9 +3,19 @@
     public static class PermissionNames
     {
         public const string Pages_Tenants = "Pages.Tenants";
-
+        
         public const string Pages_Users = "Pages.Users";
 
         public const string Pages_Roles = "Pages.Roles";
+
+
+        public const string GlobalConfiguration = "GlobalConfiguration";
+        public const string GlobalConfiguration_Users = "GlobalConfiguration.Users";
+        public const string GlobalConfiguration_Users_Create = "GlobalConfiguration.Users.Create";
+        public const string GlobalConfiguration_Users_Read = "GlobalConfiguration.Users.Read";
+        public const string GlobalConfiguration_Users_Update = "GlobalConfiguration.Users.Update";
+        public const string GlobalConfiguration_Users_Delete = "GlobalConfiguration.Users.Delete";
+
+        //public const string GlobalConfiguration_Roles = "GlobalConfiguration.Roles";
     }
 }
--- aspnet-core/src/TestProject.Core/Authorization/TestProjectAuthorizationProvider.cs
+++ aspnet-core/src/TestProject.Core/Authorization/TestProjectAuthorizationProvider.cs
@@ -1,6 +1,9 @@
-﻿using Abp.Authorization;
+﻿using System.Linq;
+using System.Reflection;
+using Abp.Authorization;
 using Abp.Localization;
 using Abp.MultiTenancy;
+using Castle.DynamicProxy.Internal;
 
 namespace TestProject.Authorization
 {
@@ -8,19 +11,74 @@ namespace TestProject.Authorization
     {
         public override void SetPermissions(IPermissionDefinitionContext context)
         {
-            var configuration = context.CreatePermission("Configuration");
-            var roles = configuration.CreateChildPermission("Configuration.Roles");
-            roles.CreateChildPermission("Configuration.Roles.Delete");
+            //var configuration = context.CreatePermission("Configuration");
+            //var roles = configuration.CreateChildPermission("Configuration.Roles");
+            //roles.CreateChildPermission("Configuration.Roles.Delete");
 
-            context.CreatePermission(PermissionNames.Pages_Users, L("Users"));
-            context.CreatePermission(PermissionNames.Pages_Roles, L("Roles"));
-            context.CreatePermission(PermissionNames.Pages_Tenants, L("Tenants"),
-                multiTenancySides: MultiTenancySides.Host);
+            //context.CreatePermission(PermissionNames.Pages_Users, L("Users"));
+            //context.CreatePermission(PermissionNames.Pages_Roles, L("Roles"));
+            //context.CreatePermission(PermissionNames.Pages_Tenants, L("Tenants"),
+            //    multiTenancySides: MultiTenancySides.Host);
+
+            MakePermissions(context);
         }
 
         private static ILocalizableString L(string name)
         {
             return new LocalizableString(name, TestProjectConsts.LocalizationSourceName);
+        }
+
+        private void MakePermissions(IPermissionDefinitionContext context)
+        {
+            Permission lastCreatedPermission = null;
+            var allPermissions = typeof(PermissionNames).GetAllFields();
+
+            foreach (var currentPermission in allPermissions)
+            {
+                string[] currentPermissionStrings = currentPermission.GetValue(currentPermission).ToString().Split(".");
+
+                var rootPermission = currentPermissionStrings.First();
+                var isCreated = context.GetPermissionOrNull(rootPermission);
+                if (isCreated == null)
+                {
+                    lastCreatedPermission = context.CreatePermission(rootPermission);
+
+                }
+                else
+                {
+                    lastCreatedPermission = isCreated;
+                }
+                if (currentPermissionStrings.Length == 1)
+                {
+                    continue;
+                }
+                foreach (var currentString in currentPermissionStrings)
+                {
+                    if (currentString == rootPermission)
+                    {
+                        continue;
+                    }
+
+                    rootPermission = rootPermission + "." + currentString;
+
+                    var isChildCreated = context.GetPermissionOrNull(rootPermission);
+                    if (isChildCreated == null)
+                    {
+                        lastCreatedPermission = context.CreatePermission(rootPermission);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                    if (currentPermissionStrings.Last() == currentString)
+                    {
+                        rootPermission = "";
+                        lastCreatedPermission = null;
+                    }
+                }
+
+            }
         }
     }
 }
